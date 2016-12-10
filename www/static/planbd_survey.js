0ce5c9fc983c0a83a6a2da71e62517e7858097a8
sabbir <bitsway@gmail.com>
2016-12-10 17:20:25 +0600

    filename

--- www/static/planbd_survey.js
+++ www/static/planbd_survey.js
@@ -1074,7 +1074,7 @@ function uploadPhotoAch(imageURI, imageName) {
 	//alert(imageURI)
     var options = new FileUploadOptions();
     options.fileKey="upload";
-    options.fileName=imageName;
+    options.filename=imageName;
     options.mimeType="image/jpeg";
 
     var params = {};
